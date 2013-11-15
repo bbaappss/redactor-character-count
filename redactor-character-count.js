a76810bd2f3eb51b98b9304d515dc9b3e99ce41a
brandon <brandonscott@gmail.com>
2013-11-15 21:53:50 +0000

    function now accepts dynamic maxChar values

--- redactor-character-count.js
+++ redactor-character-count.js
@@ -5,51 +5,55 @@
 
 (function($){
 
-  $.fn.charCount = function(){
+  $.fn.redactorCharCount = function(maxChar){
     
-    console.log('hello we are in brandon scotts plugin!');
-
     return $(this).each(function(){
 
-      // function limitText(text_field) {
-      //     if ($(text_field).text().length > 500) {
-      //         $('span.current-characters').addClass('invalid');
-      //     } else {
-      //         if ($('span.current-characters').hasClass('invalid')) {
-      //             $('span.current-characters').removeClass('invalid');
-      //         }
-      //     }
-      // }
+      // Set existing character count
 
-      // function updateCharCount(text_field) {
-      //     $('.current-characters').text($(text_field).text().length);
-      // }
+      $('.current-characters').text($('.redactor_editor').text().length);
 
-      // $('.redactor_editor').each(function() {
-      //    var elem = $(this);
+      // TODO: Write code to detect if only character in text box is a space
 
-      //    // Save current value of element
+      function limitText(text_field) {
+          if ($(text_field).text().length > maxChar) {
+              $('span.current-characters').addClass('invalid');
+          } else {
+              if ($('span.current-characters').hasClass('invalid')) {
+                  $('span.current-characters').removeClass('invalid');
+              }
+          }
+      }
 
-      //    elem.data('oldVal', elem.text());
+      function updateCharCount(text_field) {
+          $('.current-characters').text($(text_field).text().length);
+      }
 
-      //    // Bind behaviors to most methods of change
-      //    elem.bind("propertychange keyup input paste", function(event){
+      $('.redactor_editor').each(function() {
+         var elem = $(this);
 
-      //       // If value has changes
+         // Save current value of element
+
+         elem.data('oldVal', elem.text());
+
+         // Bind behaviors to most methods of change
+         elem.bind("propertychange keyup input paste", function(event){
+
+            // If value has changes
           
-      //       if (elem.data('oldVal') != elem.text()) {
+            if (elem.data('oldVal') != elem.text()) {
           
-      //        // Updated stored value
+             // Updated stored value
          
-      //        elem.data('oldVal', elem.text());
+             elem.data('oldVal', elem.text());
 
-      //        // Do action
+             // Do action
          
-      //        updateCharCount($(this));
-      //        limitText($(this));
-      //      }
-      //    });
-      //  });
+             updateCharCount($(this));
+             limitText($(this));
+           }
+         });
+       });
 
     });
     
